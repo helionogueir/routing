a476fea66740438db29a0b48e89b1b4c8aeac27c
helionogueir <helio.nogueir@gmail.com>
2016-11-21 16:47:39 -0200

    Server Request

---
+++ core/server/Request.class.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace helionogueir\rounting\request;
+
+use Exception;
+
+/**
+ * Request:
+ * - Control server request action
+ *
+ * @author Helio Nogueira <helio.nogueir@gmail.com>
+ * @version v1.0.0
+ */
+class Request {
+
+    /**
+     * Contruct basic parameters:
+     * - Get basic paramiters of match
+     * 
+     * @param helionogueir\typeBoxing\type\String $action Action of server request
+     * @param helionogueir\typeBoxing\type\String $pathRoot Path root of application
+     * @return null
+     */
+    public function __construct(String $action, String $pathRoot) {
+        return null;
+    }
+
+}
